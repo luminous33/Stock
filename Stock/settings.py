b1dd5a8caef9cdff86a80732aaf6c71df3864b3a
luminous33 <65387419+luminous33@users.noreply.github.com>
2020-10-19 00:04:49 +0900

    change localhost

--- Stock/settings.py
+++ Stock/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'investar',
         'USER': 'root',
         'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306'
     }
 }
