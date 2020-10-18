c8096eb16888c8d8619afd6e76456e20cd184e59
luminous33 <65387419+luminous33@users.noreply.github.com>
2020-10-18 19:54:50 +0900

    Add files via upload

--- index/models.py
+++ index/models.py
@@ -32,18 +32,3 @@ class DailyPrice(models.Model):
         managed = False
         db_table = 'daily_price'
         unique_together = (('code', 'date'),)
-
-class Merge(models.Model):
-    company = models.CharField(max_length=40, blank=True, null=True)
-    code = models.CharField(max_length=20)
-    date = models.DateField()
-    open = models.BigIntegerField(blank=True, null=True)
-    high = models.BigIntegerField(blank=True, null=True)
-    low = models.BigIntegerField(blank=True, null=True)
-    close = models.BigIntegerField(blank=True, null=True)
-    diff = models.BigIntegerField(blank=True, null=True)
-    volume = models.BigIntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'merge'
