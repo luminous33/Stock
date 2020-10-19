cb5178cb7d04c70d6d0c1cc1e7f94b90965668b3
luminous33 <65387419+luminous33@users.noreply.github.com>
2020-10-19 19:14:05 +0900

    Update settings.py

--- Stock/settings.py
+++ Stock/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'Investar',
         'USER': 'root',
         'PASSWORD': '1111',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306'
     }
 }
