6f77e530ce87f20a94f90c306cb6bcdc6468fecc
luminous33 <65387419+luminous33@users.noreply.github.com>
2020-10-19 14:47:51 +0900

    Add files via upload

--- DB/MarketDB.py
+++ DB/MarketDB.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pandas as pd
 # from bs4 import BeautifulSoup
 # import urllib
@@ -35,4 +36,43 @@ class MarketDB:
                                                                                                      endDate)
         df = pd.read_sql(sql, self.conn)
         df.index = df['date']
+=======
+import pandas as pd
+# from bs4 import BeautifulSoup
+# import urllib
+# from urllib.request import urlopen
+import pymysql
+# import time
+# import pandas.io.sql as sql
+from datetime import datetime
+
+
+# from threading import Timer
+# import matplotlib.pyplot as plt
+
+class MarketDB:
+    def __init__(self):
+        """생성자: MariaDB 연결 및 종목코드 딕셔너리 생성"""
+        self.conn = pymysql.connect(host='localhost', user='root', password='1111', db='Investar', charset='utf8')
+        self.codes = dict()
+        self.getCompanyInfo()
+
+    def __del__(self):
+        """소멸자: MariaDB 연결 해제"""
+        self.conn.close()
+
+    def getCompanyInfo(self):
+        """company_info 테이블에서 읽어와서 companyData와 codes에 저장"""
+        sql = "SELECT * FROM company_info"
+        companyInfo = pd.read_sql(sql, self.conn)
+        for idx in range(len(companyInfo)):
+            self.codes[companyInfo['code'].values[idx]] = companyInfo['company'].values[idx]
+
+    def getDailyPrice(self, code, startDate, endDate):
+        """daily_price 테이블에서 읽어와서 데이터프레임으로 반환"""
+        sql = "SELECT * FROM daily_price WHERE code = '{}' and date >= '{}' and date <= '{}'".format(code, startDate,
+                                                                                                     endDate)
+        df = pd.read_sql(sql, self.conn)
+        df.index = df['date']
+>>>>>>> 7c0f1678b8f7995b398f5efa3b08d1ce83c0d13c
         return df
